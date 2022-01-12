08de86c8de00eb8b30dbb417bd404026d7c1f48c
Rxanadu <ed.onuk31@gmail.com>
2022-01-12 17:08:26 -0500

    added 'BinaryCounter.cs' file to repository

---
+++ BinaryCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryCounter
+{
+    class BinaryCounter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CountBits(1234);
+        }
+
+        public static int CountBits(int n)
+        {
+            string toBinaryString = Convert.ToString(n, 2);
+            char[] binaryChars = toBinaryString.ToCharArray();
+
+            int binary1Count = 0;
+
+            foreach (char charr in binaryChars) {
+                if (charr.Equals('1')) {
+                    binary1Count++;
+                }
+            }
+
+            return binary1Count;
+        }
+    }
+}
